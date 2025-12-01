2c2d75d831ddb172f37acfec256ecc274dc000c1
Daniel La Rocque <daniellrq@outlook.com>
2025-12-01 12:02:26 -0500

    day 1 solution

--- solutions/2024/day01/solution.cpp
+++ solutions/2024/day01/solution.cpp
@@ -1,13 +1,94 @@
 #include "solution.h"
 
+#include <cassert>
+#include <cstring>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include "aoc_utils.hpp"
+#include "string.hpp"
+
+using namespace std;
+using namespace aoc::utils;
 
 namespace day01 {
 
-long long solve_part_1(const std::string& input) { return 0; }
+long long solve_part_1(const std::string& input) {
+  // R is addition
+  // L is subtraction
+  int pos = 50;
+  int res = 0;
 
-long long solve_part_2(const std::string& input) { return 0; }
-}  // namespace day01
+  const auto instructions = split(input, '\n');
+  assert(instructions.size() > 0);
+
+  for (const auto& instruction : instructions) {
+    assert(instruction.length() > 1);
+    int dist = stoi(instruction.substr(1, instruction.length() - 1).data()) % 100;
+    assert(dist > 0 & dist < 100);
+    if (instruction[0] == 'L') {  // subtract
+      if (dist > pos) {
+        pos = 100 - (dist - pos);
+      } else {
+        pos -= dist;
+      }
+    } else {  // add
+      if ((pos + dist) > 99) {
+        pos = 0 + ((dist + pos) - 100);
+      } else {
+        pos += dist;
+      }
+    }
+
+    if (pos == 0) {
+      res++;
+    }
+  }
+
+  return res;
+}
+
+long long solve_part_2(const std::string& input) {
+  // R is addition
+  // L is subtraction
+  int pos = 50;
+  int res = 0;
+
+  const auto instructions = split(input, '\n');
+
+  for (const auto& instruction : instructions) {
+    int dist = stoi(instruction.substr(1, instruction.length() - 1).data());
+    res += dist / 100;
+    dist %= 100;
+    if (instruction[0] == 'L') { // subtract
+      // We roll past 0 if the distance is greater than pos.
+      if (dist > pos) {
+        if (pos != 0) {
+          res++;
+        }
+        pos = 100 - (dist - pos);
+      } else {
+        pos -= dist;
+      }
+    } else {  // add
+      // We roll past 0 if the distance is greater than the distance between pos and 100. 
+      if ((pos + dist) > 99) {
+        pos = 0 + ((dist + pos) - 100);
+        if (pos != 0) {
+          res++;
+        }
+      } else {
+        pos += dist;
+      }
+    }
+
+    if (pos == 0) {
+      res++;
+    }
+    cout << "Instruction: " << instruction << " -> (" << pos << ", " << res << ")" << endl;
+  }
+
+  return res;
+}
+} // namespace day01
--- solutions/2024/day01/tests.cpp
+++ solutions/2024/day01/tests.cpp
@@ -1,29 +1,68 @@
 #include <gtest/gtest.h>
+
 #include <iostream>
-#include "solution.h"
+#include <filesystem>
+
 #include "aoc_utils.hpp"
+#include "solution.h"
 
 namespace {
-    const std::string EXAMPLE_INPUT = R"(
+const std::string EXAMPLE_INPUT = R"(
 )";
 }
 
 TEST(Day01, Part1_Example) {
-    // EXPECT_EQ(day01::solve_part_1(EXAMPLE_INPUT), 0);
+  const std::string input = R"(L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82)";
+  EXPECT_EQ(day01::solve_part_1(input), 3);
 }
 
 TEST(Day01, Part1_Solution) {
-    // auto input = aoc::utils::read_file(AOC_INPUT_DIR "/day01.txt");
-    // std::cout << "[SOLUTION] Part 1: " << day01::solve_part_1(input) << std::endl;
+  const std::filesystem::path dirPath = AOC_INPUT_DIR;
+  const std::filesystem::path fullPath = dirPath / "2024/day01.txt";
+  auto input = aoc::utils::read_file(fullPath);
+  std::cout << "[SOLUTION] Part 1: " << day01::solve_part_1(input) << std::endl;
 }
 
-/*
 TEST(Day01, Part2_Example) {
-    // EXPECT_EQ(day01::solve_part_2(EXAMPLE_INPUT), 0);
+  const std::string input = R"(L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82)";
+  EXPECT_EQ(day01::solve_part_2(input), 6);
+}
+
+TEST(Day01, Part2_Example2) {
+  const std::string input = R"(L68
+L30
+R48
+L5
+R60
+L55
+L1
+L399
+R14
+L82)";
+  EXPECT_EQ(day01::solve_part_2(input), 9);
 }
 
 TEST(Day01, Part2_Solution) {
-    // auto input = aoc::utils::read_file(AOC_INPUT_DIR "/day01.txt");
-    // std::cout << "[SOLUTION] Part 2: " << day01::solve_part_2(input) << std::endl;
+  const std::filesystem::path dirPath = AOC_INPUT_DIR;
+  const std::filesystem::path fullPath = dirPath / "2024/day01.txt";
+  auto input = aoc::utils::read_file(fullPath);
+  std::cout << "[SOLUTION] Part 2: " << day01::solve_part_2(input) << std::endl;
 }
-*/
