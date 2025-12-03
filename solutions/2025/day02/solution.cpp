74f3bbacd3d8b8ca57cc03136454703c0da72520
Daniel La Rocque <daniellrq@outlook.com>
2025-12-03 11:33:08 -0500

    day 2 p1

--- solutions/2025/day02/solution.cpp
+++ solutions/2025/day02/solution.cpp
@@ -1,15 +1,75 @@
 #include "solution.h"
-#include "aoc_utils.hpp"
+
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <set>
 #include <string>
+#include <utility>
 #include <vector>
+
+#include "debug_log.hpp"
+#include "string.hpp"
+
+using namespace std;
+using namespace aoc::utils;
+
+using ll = long long;
 
 namespace day02 {
 
-    long long solve_part_1(const std::string& input) {
-        return 0;
+ll expand_and_mirror(ll x) { return x + (x * powl(10, (to_string(x).length()))); }
+
+ll solve_part_1(const string& input) {
+  ll res = 0;
+  set<ll> ids{};
+  vector<string> range_strs = split(input, ',');
+  for (string& range_str : range_strs) {
+    cout << range_str << endl;
+    vector<string> temp = split(range_str, '-');
+    pair<ll, ll> range = {stoll(temp.at(0)), stoll(temp.at(1))};
+    string s1 = to_string(range.first);
+    string s2 = to_string(range.second);
+
+    if (s1.length() % 2 == 1 && s2.length() % 2 == 1) {
+      continue;
     }
 
-    long long solve_part_2(const std::string& input) {
-        return 0;
+    for (ll i = range.first; i <= range.second; i++) {
+      string i_str = to_string(i);
+      ll half = stoll(i_str.substr(0, (i_str.length() + 1) / 2));
+      ll expanded = expand_and_mirror(half);
+      // cout << i << " " << expanded << endl;
+      if (expanded >= range.first && expanded <= range.second) {
+        // cout << "found " << expanded << endl;
+        ids.insert(expanded);
+        // res += expanded;
+      }
     }
+
+    // brute force
+    // start = first two digits of start
+    // end =
+
+    /*
+        ll end = expand_and_mirror(stoll(s2.substr(0, (s2.length()) / 2)));
+        ll curr = stoll(s1.substr(0, (s1.length() + 1) / 2));
+        ll full = expand_and_mirror(curr);
+        cout << "curr: " << curr << ", full: " << full << ", end: " << end << endl;
+        while (full <= end) {
+          cout << "[" << curr << " -> " << full << "]" << endl;
+          if (full >= range.first && full <= range.second) {
+            cout << "  INVALID ID: " << full << endl;
+            res += full;
+          }
+          curr++;
+          full = expand_and_mirror(curr);
+        }
+      }
+    */
+  }
+  return accumulate(ids.begin(), ids.end(), 0LL);
 }
+
+long long solve_part_2(const std::string& input) { return 0; }
+}  // namespace day02
--- solutions/2025/day02/tests.cpp
+++ solutions/2025/day02/tests.cpp
@@ -1,29 +1,37 @@
 #include <gtest/gtest.h>
+
 #include <iostream>
-#include "solution.h"
+
 #include "aoc_utils.hpp"
+#include "solution.h"
 
 namespace {
-    const std::string EXAMPLE_INPUT = R"(
+const std::string EXAMPLE_INPUT = R"(
 )";
 }
 
 TEST(Day02, Part1_Example) {
-    // EXPECT_EQ(day02::solve_part_1(EXAMPLE_INPUT), 0);
+  const std::string input =
+      R"(11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124)";
+  EXPECT_EQ(day02::solve_part_1(input), 1227775554);
 }
 
 TEST(Day02, Part1_Solution) {
-    // auto input = aoc::utils::read_file(AOC_INPUT_DIR "/day02.txt");
-    // std::cout << "[SOLUTION] Part 1: " << day02::solve_part_1(input) << std::endl;
+  const std::filesystem::path dirPath = AOC_INPUT_DIR;
+  const std::filesystem::path fullPath = dirPath / "2025/day02.txt";
+  auto input = aoc::utils::read_file(fullPath);
+  EXPECT_EQ(day02::solve_part_1(input), 0);
 }
 
-/*
 TEST(Day02, Part2_Example) {
-    // EXPECT_EQ(day02::solve_part_2(EXAMPLE_INPUT), 0);
+  const std::string input =
+      R"(11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124)";
+  EXPECT_EQ(day02::solve_part_2(input), 4174379265);
 }
 
 TEST(Day02, Part2_Solution) {
-    // auto input = aoc::utils::read_file(AOC_INPUT_DIR "/day02.txt");
-    // std::cout << "[SOLUTION] Part 2: " << day02::solve_part_2(input) << std::endl;
+  const std::filesystem::path dirPath = AOC_INPUT_DIR;
+  const std::filesystem::path fullPath = dirPath / "2025/day02.txt";
+  auto input = aoc::utils::read_file(fullPath);
+  EXPECT_EQ(day02::solve_part_2(input), 0);
 }
-*/
